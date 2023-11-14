fda2e9a1a19d9755e2a62e6b3d63484c62f2a8d2
Arman <73093255+Ar0manKhan@users.noreply.github.com>
2023-11-14 20:48:34 +0530

    feat: navbar setup

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,44 +1,38 @@
+import { getServerSession } from "next-auth"
+import Link from "next/link"
+import Image from "next/image"
+
 export function Navbar() {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <Link className="btn btn-ghost text-xl" href="/">Study List</Link>
+      </div>
+      <AuthButtons />
+    </div >
+  )
+}
+
+async function AuthButtons() {
+  const session = await getServerSession()
+  if (session?.user) {
+    return (
+      <div className="navbar-end">
+        {/* <Link className="btn" href="/api/auth/signout">Sign Out</Link> */}
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0}>
+            <Image src={session.user.image || ""} alt="Profile" width={32} height={32} className="rounded-full" />
           </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><Link href="/api/auth/signout">Sign Out</Link></li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        {/* <a className="btn" onClick={() => signIn()}>Sign In</a> */}
-        <a className="btn">Sign In</a>
-      </div>
+    )
+  }
+  return (
+    <div className="navbar-end">
+      <Link className="btn" href="/api/auth/signin">Sign In</Link>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
-import { getServerSession } from 'next-auth';
-// import { authOptions } from 'app/auth';
 import { Navbar } from './components/navbar';
-
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,12 +13,10 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode,
 }) {
-  const session = await getServerSession()
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <pre>{JSON.stringify(session)}</pre>
         {children}
       </body>
     </html>
